525cd1e470affff36569486fc61843460ff5b956
EastAsianInc <js4cap@gmail.com>
2025-03-02 10:35:25 -0600

    Added variables to hold question answers

--- app.js
+++ app.js
@@ -7,49 +7,90 @@ while(userName == false)
     }
 console.log("Hello, " + userName + "!");
 
-let userAnswer = '';
+let firstQuestion = '';
+let secondQuestion = '';
+let thirdQuestion = '';
+let fourthQuestion = '';
+let fifthQuestion = '';
 
-while(userAnswer != 12)
+while(firstQuestion != 12)
 {
-    userAnswer = input.questionInt(`What would the integer value be for the following code?: \n
+    firstQuestion = input.questionInt(`What would the integer value be for the following code?: \n
         console.log(5 + 7);
         \nYour Response: `);
-        if(userAnswer == 12)
+        if(firstQuestion == 12)
             {
                 continue;
             }
         else
         {
-            console.log("Not quite, try again.");
+            console.log("You answered: " + firstQuestion + ", which is incorrect, try again.");
         }
 }
 
-while(userAnswer != 'number')
+while(secondQuestion != 'number')
     {
-        userAnswer = input.question(`What is the data type of the variable below? (No capital letters)
+        secondQuestion = input.question(`What is the data type of the variable below? (No capital letters)
             let mysteryData = 7;
             \nYour Response: `);
-            if(userAnswer == 'number')
+            if(secondQuestion == 'number')
                 {
                     continue;
                 }
             else
             {
-                console.log("Not quite, try again.");
+                console.log("You answered: " + secondQuestion + ", which is incorrect, try again.");
             }
     }
 
-    while(userAnswer != 'string')
+    while(thirdQuestion != 'string')
         {
-            userAnswer = input.question(`What is the data type of the variable below? (No capital letters)
+            thirdQuestion = input.question(`What is the data type of the variable below? (No capital letters)
                 let mysteryData = 'I am a mystery';
                 \nYour Response: `);
-                if(userAnswer == 'string')
+                if(thirdQuestion == 'string')
                     {
                         continue;
                     }
                 else
                 {
-                    console.log("Not quite, try again.");
+                    console.log("You answered: " + thirdQuestion + ", which is incorrect, try again.");
                 }
         }
+
+    while(fourthQuestion != 'object')
+    {
+        fourthQuestion = input.question(`What is the data type of the variable below? (No capital letters)
+            const person = {firstName:"John", lastName:"Doe"};
+            \nYour Response: `);
+            if(fourthQuestion == 'object')
+            {
+                continue;
+            }
+            else
+            {
+                console.log("You answered: " + fourthQuestion + ", which is incorrect, try again.");
+            }
+    }
+
+    while(fifthQuestion != 'array')
+    {
+        fifthQuestion = input.question(`What is the data type of the variable below? (No capital letters)
+            const fruits = ["pineapple", "cherry", "apples", "mangos"];
+            \nYour Response: `);
+            if(fifthQuestion == 'array')
+            {
+                continue;
+            }
+            else
+            {
+                console.log("You answered: " + fifthQuestion + ", which is incorrect, try again.");
+            }
+    }
+    console.log(`Your answers:
+        1. ${firstQuestion}
+        2. ${secondQuestion}
+        3. ${thirdQuestion}
+        4. ${fourthQuestion}
+        5. ${fifthQuestion}
+        `);
